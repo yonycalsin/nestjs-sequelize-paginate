15c334a70925cb4027b5af01e6f909ed0fa112f9
yonicb <yonicb20@gmail.com>
2020-02-28 23:38:52 -0500

    data: 🗂️ Aggregated all tokens for inject !

---
+++ lib/paginate.constans.ts
@@ -0,0 +1,7 @@
+/**
+ * Injection tokens
+ */
+export const PAGINATION_SERVICE_TOKEN = Symbol('PAGINATE_SERVICE');
+export const PAGINATION_TOKEN = 'PAGINATION_TOKEN';
+export const PAGINATION_LOADER = 'PAGINATION_LOADER';
+export const PAGINATION_OPTIONS = 'PAGINATION_OPTIONS';
