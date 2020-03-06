6de9c0ef55c1da8d3c03cacae3e53795255e8d0a
Yoni Calsin <yonicb20@gmail.com>
2020-03-05 22:30:47 -0500

    fix: 🐛 Removed unnesesary valitions !

--- lib/paginate.decorator.ts
+++ lib/paginate.decorator.ts
@@ -4,29 +4,16 @@ export const PaginateQuery = createParamDecorator((param: 'all', input) => {
    const {
       route: { path },
       query,
-      url,
    } = input;
+
    const offset: any = query.offset ? parseFloat(query.offset) : null;
    const page: any = query.page ? parseFloat(query.page) : null;
 
    let payload: { [key: string]: any } = {
-      [path && 'path']: path,
-      [url && 'url']: url,
-      [page && 'page']: page,
-      [offset && 'offset']: offset,
-      origin: 'paginate.query',
+      path: path,
+      page: page,
+      offset: offset,
+      showOffset: param === 'all' || false,
    };
-   if (param === 'all') {
-      payload['allowOffset'] = true;
-   }
-
-   if (typeof param === 'string') {
-      if (param == 'all') {
-         payload = {
-            ...payload,
-            [offset && 'offset']: offset,
-         };
-      }
-   }
    return payload;
 });
